79ef21d75d2d1b8eab5122956a3376df92b31796
Yassine <medyassinebenromdhane00@gmail.com>
2024-05-22 19:48:02 +0100

    first commit

---
+++ Models/Absences.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace gestionabscence.Models
+{
+    public class Absences
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AbsenceId { get; set; }
+
+        [Required]
+        public int HoursNumber { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
